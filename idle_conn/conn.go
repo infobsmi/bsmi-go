dc1d2e014d6ae6bccdceafb6f4ea3400eff511d5
tesw yew isal <278153+cnmade@users.noreply.github.com>
2022-07-11 21:58:41 +0800

    Update logger

--- idle_conn/conn.go
+++ idle_conn/conn.go
@@ -1,17 +1,24 @@
 package idle_conn
 
 import (
+	"log"
 	"net"
+	"sync"
+	"sync/atomic"
 	"time"
 )
 
 const IdleTimeout = 120 * time.Second
 
+var logger = log.Default()
+
 type ValidConn interface {
 	net.Conn
 }
 type IdleConn[T ValidConn] struct {
-	Conn T
+	Conn   T
+	mt     sync.Mutex
+	LastTs int64
 }
 
 func (ic *IdleConn[T]) Read(buf []byte) (int, error) {
@@ -20,7 +27,24 @@ func (ic *IdleConn[T]) Read(buf []byte) (int, error) {
 }
 
 func (ic *IdleConn[T]) UpdateIdleTime(t time.Time) {
-	_ = ic.Conn.SetDeadline(t)
+	if ic.mt.TryLock() {
+		defer ic.mt.Unlock()
+
+		tsNow := time.Now()
+		lastTs := atomic.LoadInt64(&ic.LastTs)
+		if lastTs > tsNow.Unix() {
+			return
+		}
+		//老的时间
+		tsNext := tsNow.Add(4 * time.Second)
+		tsRenew := tsNext.Add(IdleTimeout)
+		atomic.StoreInt64(&ic.LastTs, tsNext.Unix())
+
+		logger.Printf("获取到锁，且应该更新，oldTs: %+v, 更新超时时间: %+v\n", ic.LastTs, tsRenew)
+		_ = ic.Conn.SetReadDeadline(tsRenew)
+		_ = ic.Conn.SetWriteDeadline(tsRenew)
+
+	}
 }
 
 func (ic *IdleConn[T]) Write(buf []byte) (int, error) {
